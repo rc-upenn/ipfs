02724e9b3446ebbc4ddc0999b9765e9c8bd1798e
rc-upenn <rwille@seas.upenn.edu>
2026-03-02 22:23:34 +0000

    Update ipfs.py
    fixes 1

--- ipfs.py
+++ ipfs.py
@@ -3,13 +3,30 @@ import json
 
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
-	#YOUR CODE HERE
+	jwt = os.getenv("PINATA_JWT")
+	if not jwt:
+        raise RuntimeError("PINATA_JWT not set, so cid can’t be created.")
 
-	return cid
+    r = requests.post(
+        "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+        headers={"Authorization": f"Bearer {jwt}", "Content-Type": "application/json"},
+        json=data,
+        timeout=30
+    )
+    r.raise_for_status()
+
+    cid = r.json()["IpfsHash"]
+    return cid
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
-	#YOUR CODE HERE	
+	r = requests.get(f"https://ipfs.io/ipfs/{cid}", timeout=30)
+    r.raise_for_status()
 
-	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
-	return data
+    if content_type == "json":
+        data = r.json()
+    else:
+        data = json.loads(r.content.decode("utf-8"))
+
+    assert isinstance(data, dict), "get_from_ipfs should return a dict"
+    return data
